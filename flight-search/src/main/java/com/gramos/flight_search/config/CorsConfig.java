18a42dc8930bc255f9767df458124bd8be8cdcf1
Gerardo Ramos <gerardo.ramos@encora.com>
2025-05-07 14:01:20 -0600

    Created CorsConfig

---
+++ flight-search/src/main/java/com/gramos/flight_search/config/CorsConfig.java
@@ -0,0 +1,22 @@
+package com.gramos.flight_search.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Permite todas las rutas
+                        .allowedOrigins("http://localhost:8080") // Permite solicitudes desde React
+                        .allowedMethods("GET", "POST", "PUT", "DELETE") // Métodos permitidos
+                        .allowedHeaders("*"); // Permitir todos los encabezados
+            }
+        };
+    }
+}
